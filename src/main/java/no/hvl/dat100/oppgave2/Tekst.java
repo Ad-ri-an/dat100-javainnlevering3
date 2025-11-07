1f48ca8ede49b2af4a4a66b540dab34f0394cf53
Table Produshcer <adaus1505@hvl.no>
2025-11-07 01:08:13 +0100

    oppgave 2

--- src/main/java/no/hvl/dat100/oppgave2/Tekst.java
+++ src/main/java/no/hvl/dat100/oppgave2/Tekst.java
@@ -5,33 +5,53 @@ import no.hvl.dat100.oppgave1.*;
 
 public class Tekst extends Innlegg {
 
-	// TODO: objektvariable 
+	private String tekst;
 	
 	public Tekst () {
 		
 	}
 	
 	public Tekst(int id, String bruker, String dato, String tekst) {
+
+        super(id, bruker, dato);
+        this.tekst = tekst;
+
 		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
 	public Tekst(int id, String bruker, String dato, int likes, String tekst) {
+
+        super(id, bruker, dato, likes);
+        this.tekst = tekst;
+
 		throw new UnsupportedOperationException(TODO.constructor("Tekst"));
 	}
 	
 	public String getTekst() {
-		throw new UnsupportedOperationException(TODO.method());
 
+        return tekst;
+
+		throw new UnsupportedOperationException(TODO.method());
 	}
 
 	public void setTekst(String tekst) {
-		throw new UnsupportedOperationException(TODO.method());
+
+        this.tekst = tekst;
+
+        throw new UnsupportedOperationException(TODO.method());
 	}
 
 	@Override
 	public String toString() {
-		throw new UnsupportedOperationException(TODO.method());
 
+        return "TEKST\n" +
+                getId() + "\n" +
+                getBruker() + "\n" +
+                getDato() + "\n" +
+                getLikes() + "\n" +
+                tekst + "\n";
+
+        throw new UnsupportedOperationException(TODO.method());
 	}
 	
 	// Metoden nedenfor er kun for valgfri oppgave 6
