d15183d5ee751d6a855edca6d0b2f53f32d01b3d
Table Produshcer <adaus1505@hvl.no>
2025-11-11 12:47:06 +0100

    oppagve 5

--- src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
+++ src/main/java/no/hvl/dat100/oppgave5/LesBlogg.java
@@ -6,6 +6,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.Buffer;
 import java.util.Scanner;
 
 import no.hvl.dat100.common.TODO;
@@ -22,7 +23,37 @@ public class LesBlogg {
 
 	public static Blogg les(String mappe, String filnavn) {
 
-		throw new UnsupportedOperationException(TODO.method());
+        Blogg samling = null;
+        try (BufferedReader reader = new BufferedReader(new FileReader(new File(mappe, filnavn)))) {
+            int antall = Integer.parseInt(reader.readLine());
+            samling = new Blogg(antall);
 
+            for (int i = 0; i < antall; i++) {
+                String type = reader.readLine();
+                int id = Integer.parseInt(reader.readLine());
+                String bruker = reader.readLine();
+                String dato = reader.readLine();
+                int likes = Integer.parseInt(reader.readLine());
+                String tekst = reader.readLine();
+
+                Innlegg innlegg;
+
+                if (type.equals(TEKST)) {
+                    innlegg = new Tekst(id, bruker, dato, likes, tekst);
+                } else {
+                    String url = reader.readLine();
+                    innlegg = new Bilde(id, bruker, dato, likes, tekst, url);
+                }
+
+                samling.leggTilUtvid(innlegg);
+            }
+
+        } catch (IOException e) {
+            System.out.println("Feil ved lesing av fil: " + e.getMessage());
+        }
+
+        return samling;
+
+		//throw new UnsupportedOperationException(TODO.method());
 	}
 }
